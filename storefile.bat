f5e549d3fde79995586f5b20b09d20fe6f7ba40e
jesko <rattle@nullteilerfrei.de>
2022-03-21 13:31:33 +0100

    adjusts store script

--- storefile.bat
+++ storefile.bat
@@ -1,1 +1,3 @@
-emit "%1" [|cm -2| aes -RmCBC REFINERYTESTDATA | dump -t {sha256!r}.enc | aes -mCBC REFINERYTESTDATA | sha256 -t | cfmt {sha256!r}\n{} ]]
+@echo off
+emit "%1" [| put id sha256 | aes -RmCBC REFINERYTESTDATA | dump -t {id}.enc | aes -mCBC REFINERYTESTDATA | sha256 -t | cfmt {id}\n{} ]]
+del "%1"
