c0b9dca9c55b4caa5a55f4d207cfa7eb457a7ca7
jesko <rattle@nullteilerfrei.de>
2023-11-19 17:28:59 +0100

    adds support for custom key in decode

--- _decode.bat
+++ _decode.bat
@@ -1,2 +1,3 @@
 @echo off
-emit "%1" [| aes -mCBC REFINERYTESTDATA | dump -t {sha256} | cfmt {sha256} ]]
+if "%2"=="" (set key=REFINERYTESTDATA) else (set key=%2)
+emit "%1" [| aes -mCBC %key% | dump -t {sha256} | cfmt {sha256} ]]
