0503b5f24ae61e5bbaeff180064fed0610796fba
jesko <rattle@nullteilerfrei.de>
2025-09-04 23:35:00 +0200

    rename cfmt unit in scripts

--- _decode.bat
+++ _decode.bat
@@ -1,3 +1,3 @@
 @echo off
 if "%2"=="" (set key=REFINERYTESTDATA) else (set key=%2)
-emit "%1" [| aes -mCBC %key% | dump -t {sha256} | cfmt {sha256} ]]
+emit "%1" [| aes -mCBC %key% | dump -t {sha256} | pf {sha256} ]]
